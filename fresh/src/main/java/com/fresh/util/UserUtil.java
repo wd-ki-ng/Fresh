ed55f15d2e9bbe4c7aadc9a58924755ebdf8412b
ki_ng <eowhd171@naver.com>
2024-12-03 12:43:21 +0900

    Revert "Merge branch 'Park' into developer"
    This reverts commit 0450c9efa2345e5dd3e7b4553206c90ad32ee1b1, reversing
    changes made to e8a37632f49c64e827501d843f3430538116a16e.
    
    # Conflicts:
    #	fresh/src/main/java/com/fresh/controller/LoginContoller.java

--- fresh/src/main/java/com/fresh/util/UserUtil.java
+++ fresh/src/main/java/com/fresh/util/UserUtil.java
@@ -16,14 +16,15 @@ import com.fresh.repository.LoginRepository;
 
 @Component
 public class UserUtil {
+	
 	@Autowired
 	private LoginRepository loginRepository;
 
 	public String getUsername() {
 		// 현재 사용자 아이디
-		String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+		String id = SecurityContextHolder.getContext().getAuthentication().getName();
 
-		return user_id;
+		return id;
 
 	}
 
@@ -34,39 +35,39 @@ public class UserUtil {
 		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 		Iterator<? extends GrantedAuthority> iter = authorities.iterator();
 		GrantedAuthority auth = iter.next();
-		String ROLE = auth.getAuthority();
+		String role = auth.getAuthority();
 		
-		return ROLE;
+		return role;
 	}
 	
 	public UserDTO getUserNameAndRole() {
-		String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+		String id = SecurityContextHolder.getContext().getAuthentication().getName();
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 		Iterator<? extends GrantedAuthority> iter = authorities.iterator();
 		GrantedAuthority auth = iter.next();
-		String ROLE = auth.getAuthority();
+		String role = auth.getAuthority();
 		
 		UserDTO user = new UserDTO();
-		user.setUser_id(user_id);
-		user.setROLE(ROLE);
+		user.setUser_id(id);
+		user.setRole(role);
 		return user;
 	}
 	
 	public UserDTO getUserData() {
-		String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+		String id = SecurityContextHolder.getContext().getAuthentication().getName();
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 		Iterator<? extends GrantedAuthority> iter = authorities.iterator();
 		GrantedAuthority auth = iter.next();
-		String ROLE = auth.getAuthority();
+		String role = auth.getAuthority();
 		
 		UserDTO user = new UserDTO();
-		user.setUser_id(user_id);
-		user.setROLE(ROLE);
-		if (user.getROLE().equals("ROLE_ANONYMOUS")) {
+		user.setUser_id(id);
+		user.setRole(role);
+		if (user.getRole().equals("ROLE_ANONYMOUS")) {
 			return null;
 		} else {
 			return loginRepository.getUserData(user);
