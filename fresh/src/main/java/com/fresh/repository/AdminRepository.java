77d6f193e869c40243a354a1262dcd99b8b49c06
hamtaro700 <jieunzzang1998@gmail.com>
2024-12-20 14:31:40 +0900

    어드민 서비스랑 리포지토리 기능별로 구분하고 주석 작성

--- fresh/src/main/java/com/fresh/repository/AdminRepository.java
+++ fresh/src/main/java/com/fresh/repository/AdminRepository.java
@@ -16,83 +16,93 @@ public class AdminRepository {
 	@Autowired
 	private SqlSession sqlSession;
 
-	public List<UserDTO> getMemList() {
+	// --------------------------------------会員管理--------------------------------------
+	public List<UserDTO> getMemList() {					// 全ての会員リスト
 		return sqlSession.selectList("admin.mem_list");
 	}
 	
-	public UserDTO getOneMem(Long user_no) {
+	public UserDTO getOneMem(Long user_no) {			// 特定の会員情報
 		return sqlSession.selectOne("admin.getMem", user_no);
 	}
 	
-	public List<BoardDTO> getMemPosts(Long user_no) {
+	public List<BoardDTO> getMemPosts(Long user_no) {	// 特定の会員が書いたポストリスト
 		return sqlSession.selectList("admin.memPosts", user_no);
 	}
 	
-	public void setOneMem(UserDTO member) {
+	public void setOneMem(UserDTO member) {				// 特定の会員情報の修正
 		sqlSession.update("admin.memUpdate", member);
 	}
 	
-	public void delOneMem(Long user_no) {
+	public void delOneMem(Long user_no) {				// 特定の会員脱退
 		sqlSession.delete("admin.memDelete", user_no);
 	}
+	// --------------------------------------会員管理--------------------------------------
 	
-	public List<BoardDTO> getAllPosts() {
+	// ----------------------------------------ポスト管理----------------------------------------
+	public List<BoardDTO> getAllPosts() {				// 削除されていないポストリスト
 		return sqlSession.selectList("admin.post_list");
 	}
 	
-	public int boardUpdate(BoardDTO board) {
+	public int boardUpdate(BoardDTO board) {			// 管理者のポスト修正
 		return sqlSession.update("admin.boardUpdate", board);
 	}
 	 
-	public BoardDTO findById(long no) {
+	public BoardDTO findById(long no) {					// 修正しようとしているポストの情報を探す
 		return sqlSession.selectOne("admin.findById", no);
 	}
 	
-	public void setOneBoard_del(Long board_no) {
+	public void setOneBoard_del(Long board_no) {		// 特定のポストの削除
 		sqlSession.update("admin.postDel", board_no);
 	}
+	// ----------------------------------------ポスト管理----------------------------------------
 	
-	public List<CommentDTO> getAllComments() {
+	// ----------------------------------------コメント管理----------------------------------------
+	public List<CommentDTO> getAllComments() {			// 全てのコメントリスト
 		return sqlSession.selectList("admin.com_list");
 	}
 	
-	public void setOneCom_del(Long com_no) {
+	public void setOneCom_del(Long com_no) {			// コメント削除
 		sqlSession.update("admin.comDel", com_no);
 	}
+	// ----------------------------------------コメント管理----------------------------------------
 
-	public List<BoardDTO> getAllNotices() {
+	// ----------------------------------------お知らせ管理----------------------------------------
+	public List<BoardDTO> getAllNotices() {				// お知らせリスト
 		return sqlSession.selectList("admin.noti_list");
 	}
 	
-	public void setNotice(BoardDTO notice) {
+	public void setNotice(BoardDTO notice) {			// お知らせ作成
 		sqlSession.insert("admin.noti_write", notice);
 	}
+	// ----------------------------------------お知らせ管理----------------------------------------
 
-	public List<BoardDTO> getDelPosts() {
+	// -----------------------------------------ゴミ箱管理-----------------------------------------
+	public List<BoardDTO> getDelPosts() {				// 削除されたポストのリスト
 		return sqlSession.selectList("admin.del_post");
 	}
 	
-	public void restorePost(Long board_no) {
+	public List<CommentDTO> getDelComs() {				// 削除されたコメントのリスト
+		return sqlSession.selectList("admin.del_com");
+	}
+	
+	public List<BoardDTO> getDelNotis() {				// 削除されたお知らせのリスト
+		return sqlSession.selectList("admin.del_noti");
+	}
+	
+	public void restorePost(Long board_no) {			// 削除されたポストの復旧
 		sqlSession.update("admin.retorePost", board_no);
 	}
 	
-	public void restoreComment(Long com_no) {
+	public void restoreComment(Long com_no) {			// 削除されたコメントの復旧
 		sqlSession.update("admin.retoreCom", com_no);
 	}
 	
-	public void eliminatePost(Long board_no) {
+	public void eliminatePost(Long board_no) {			// 削除されたポストの永久削除
 		sqlSession.delete("admin.eliminatePost", board_no);
 	}
 	
-	public void eliminateComment(Long com_no) {
+	public void eliminateComment(Long com_no) {			// 削除されたコメントの永久削除
 		sqlSession.delete("admin.eliminateCom", com_no);
 	}
-
-	public List<CommentDTO> getDelComs() {
-		return sqlSession.selectList("admin.del_com");
-	}
-
-	public List<BoardDTO> getDelNotis() {
-		return sqlSession.selectList("admin.del_noti");
-	}
+	// -----------------------------------------ゴミ箱管理-----------------------------------------
 }
--- fresh/src/main/java/com/fresh/service/AdminService.java
+++ fresh/src/main/java/com/fresh/service/AdminService.java
@@ -16,88 +16,95 @@ public class AdminService {
 	@Autowired
 	private AdminRepository adminRepository;
 	
-	public List<UserDTO> getMemList() {
+	// --------------------------------------会員管理--------------------------------------
+	public List<UserDTO> getMemList() {					// 全ての会員リスト
 		return adminRepository.getMemList();
 	}
 	
-	public UserDTO getOneMem(Long user_no) {
+	public UserDTO getOneMem(Long user_no) {			// 特定の会員情報
 		return adminRepository.getOneMem(user_no);
 	}
 	
-	public List<BoardDTO> getMemPosts(Long user_no) {
+	public List<BoardDTO> getMemPosts(Long user_no) {	// 特定の会員が書いたポストリスト
 		return adminRepository.getMemPosts(user_no);
 	}
 	
-	public void setOneMem(UserDTO member) {
+	public void setOneMem(UserDTO member) {				// 特定の会員情報の修正
 		adminRepository.setOneMem(member);
 	}
 	
-	public void delOneMem(Long user_no) {
+	public void delOneMem(Long user_no) {				// 特定の会員脱退
 		adminRepository.delOneMem(user_no);
 	}
+	// --------------------------------------会員管理--------------------------------------
 	
-	public List<BoardDTO> getAllPosts() {
+	// ----------------------------------------ポスト管理----------------------------------------
+	public List<BoardDTO> getAllPosts() {				// 削除されていないポストリスト
 		return adminRepository.getAllPosts();
 	}
 	
-	public int boardUpdate(BoardDTO board) {
+	public int boardUpdate(BoardDTO board) {			// 管理者のポスト修正
 		return adminRepository.boardUpdate(board);
-		
 	}
 
-	public BoardDTO findById(Long no) {
+	public BoardDTO findById(Long no) {					// 修正しようとしているポストの情報を探す
 	    return adminRepository.findById(no);
 	}
 	
-	public void setOneBoard_del(Long board_no) {
+	public void setOneBoard_del(Long board_no) {		// 特定のポストの削除
 		adminRepository.setOneBoard_del(board_no);
 	}
+	// ----------------------------------------ポスト管理----------------------------------------
 	
-	public List<CommentDTO> getAllComments() {
+	// ----------------------------------------コメント管理----------------------------------------
+	public List<CommentDTO> getAllComments() {			// 全てのコメントリスト
 		return adminRepository.getAllComments();
 	}
 	
-	public void setOneCom_del(Long com_no) {
+	public void setOneCom_del(Long com_no) {			// コメント削除
 		adminRepository.setOneCom_del(com_no);
 	}
-
-	public List<BoardDTO> getAllNotices() {
+	// ----------------------------------------コメント管理----------------------------------------
+	
+	// ----------------------------------------お知らせ管理----------------------------------------
+	public List<BoardDTO> getAllNotices() {				// お知らせリスト
 		return adminRepository.getAllNotices();
 	}
 	
-	// ポスト作成
-	public void setNotice(BoardDTO notice, Long no, String name) {
+	public void setNotice(BoardDTO notice, Long no, String name) {		// お知らせ作成
 		notice.setUser_no(no);
 		notice.setBoard_write(name);
 		adminRepository.setNotice(notice);
 	}
-
-	public List<BoardDTO> getDelPosts() {
+	// ----------------------------------------お知らせ管理----------------------------------------
+	
+	// -----------------------------------------ゴミ箱管理-----------------------------------------
+	public List<BoardDTO> getDelPosts() {				// 削除されたポストのリスト
 		return adminRepository.getDelPosts();
 	}
 	
-	public void restoreComment(Long com_no) {
-		adminRepository.restoreComment(com_no);
-	}
-	
-	public void restorePost(Long board_no) {
-		adminRepository.restorePost(board_no);
-	}
-
-	public List<CommentDTO> getDelComs() {
+	public List<CommentDTO> getDelComs() {				// 削除されたコメントのリスト
 		return adminRepository.getDelComs();
 	}
 	
-	public void eliminatePost(Long board_no) {
+	public List<BoardDTO> getDelNotis() {				// 削除されたお知らせのリスト
+		return adminRepository.getDelNotis();
+	}
+	
+	public void restorePost(Long board_no) {			// 削除されたポストの復旧
+		adminRepository.restorePost(board_no);
+	}
+	
+	public void restoreComment(Long com_no) {			// 削除されたコメントの復旧
+		adminRepository.restoreComment(com_no);
+	}
+	
+	public void eliminatePost(Long board_no) {			// 削除されたポストの永久削除
 		adminRepository.eliminatePost(board_no);
 	}
 	
-	public void eliminateComment(Long com_no) {
+	public void eliminateComment(Long com_no) {			// 削除されたコメントの永久削除
 		adminRepository.eliminateComment(com_no);
 	}
-
-	public List<BoardDTO> getDelNotis() {
-		return adminRepository.getDelNotis();
-	}
-
+	// -----------------------------------------ゴミ箱管理-----------------------------------------
 }
